3347efd3501e9128f92c16501ccf292013a80e9c
YuChia <h42279@gmail.com>
2025-08-03 12:36:37 +0800

    add shared kernel project (by gemini cli, mode: 2.5-pro)

---
+++ src/Shared.Kernel/Abstractions/IAggregateRoot.cs
@@ -0,0 +1,5 @@
+namespace Shared.Kernel.Abstractions;
+
+public interface IAggregateRoot : IEntity
+{
+}
---
+++ src/Shared.Kernel/Abstractions/IEntity.cs
@@ -0,0 +1,6 @@
+namespace Shared.Kernel.Abstractions;
+
+public interface IEntity
+{
+    public object[] GetKeys();
+}
