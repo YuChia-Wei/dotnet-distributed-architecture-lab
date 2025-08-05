14a743b6270dae969c3e7dc5d4eda30ef08fc22a
YuChia <h42279@gmail.com>
2025-08-05 21:01:31 +0800

    手工建立領域事件的發布者

---
+++ src/Order/DomainCore/SaleOrders.Infrastructure/BuildingBlocks/DomainEventDispatcher.cs
@@ -0,0 +1,21 @@
+﻿using Lab.BuildingBlocks.Domains;
+
+namespace SaleOrders.Infrastructure.BuildingBlocks;
+
+/// <inheritdoc />
+/// <remarks>
+/// 由於涉及 IO，因此由 infra 端實作
+/// </remarks>
+public class DomainEventDispatcher : IDomainEventDispatcher
+{
+    /// <summary>
+    /// 發布領域事件
+    /// </summary>
+    /// <param name="events"></param>
+    /// <param name="ct"></param>
+    /// <returns></returns>
+    public Task DispatchAsync(IEnumerable<IDomainEvent> events, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+}
