9c8330a4f0cdeb68aa7fec969c7f7f16c2f6f2f4
YuChia <h42279@gmail.com>
2025-08-19 22:36:34 +0800

    整理實作，將 product 的 handler 依據 command / query 的不同類型進行分類

--- src/Product/DomainCore/SaleProducts.Applications/Commands/ProductCommandHandler.cs
+++ src/Product/DomainCore/SaleProducts.Applications/Commands/ProductCommandHandler.cs
@@ -1,11 +1,10 @@
 using SaleProducts.Applications.Dtos;
-using SaleProducts.Applications.Queries;
 using SaleProducts.Applications.Repositories;
 using SaleProducts.Domains;
 
 namespace SaleProducts.Applications.Commands;
 
-public class ProductHandler
+public class ProductCommandHandler
 {
     public static async Task<ProductDto> Handle(CreateProductCommand command, IProductDomainRepository repository)
     {
@@ -35,21 +34,5 @@ public class ProductHandler
         }
 
         await repository.DeleteAsync(command.Id);
-    }
-
-    public static async Task<IEnumerable<ProductDto>> Handle(GetAllProductsQuery query, IProductDomainRepository repository)
-    {
-        var products = await repository.GetAllAsync();
-        return products.Select(p => new ProductDto(p.Id, p.Name, p.Description, p.Price, p.Stock));
-    }
-
-    public static async Task<ProductDto> Handle(GetProductByIdQuery query, IProductDomainRepository repository)
-    {
-        var product = await repository.GetByIdAsync(query.Id);
-        if (product == null)
-        {
-            throw new KeyNotFoundException($"Product with ID {query.Id} not found.");
-        }
-        return new ProductDto(product.Id, product.Name, product.Description, product.Price, product.Stock);
     }
 }
---
+++ src/Product/DomainCore/SaleProducts.Applications/Queries/ProductQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using SaleProducts.Applications.Dtos;
+using SaleProducts.Applications.Repositories;
+
+namespace SaleProducts.Applications.Queries;
+
+public class ProductQueryHandler
+{
+    public static async Task<IEnumerable<ProductDto>> Handle(GetAllProductsQuery query, IProductDomainRepository repository)
+    {
+        var products = await repository.GetAllAsync();
+        return products.Select(p => new ProductDto(p.Id, p.Name, p.Description, p.Price, p.Stock));
+    }
+
+    public static async Task<ProductDto> Handle(GetProductByIdQuery query, IProductDomainRepository repository)
+    {
+        var product = await repository.GetByIdAsync(query.Id);
+        if (product == null)
+        {
+            throw new KeyNotFoundException($"Product with ID {query.Id} not found.");
+        }
+        return new ProductDto(product.Id, product.Name, product.Description, product.Price, product.Stock);
+    }
+}
