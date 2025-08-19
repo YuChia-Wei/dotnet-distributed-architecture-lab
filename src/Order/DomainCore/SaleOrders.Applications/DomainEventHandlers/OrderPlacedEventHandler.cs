ac0c8e9d3134c5e7e0ac1e18518395563f159f60
YuChia <h42279@gmail.com>
2025-08-19 22:36:58 +0800

    OrderPlacedEventHandler 中的方法也改為靜態方法，並修正命名

--- src/Order/DomainCore/SaleOrders.Applications/DomainEventHandlers/OrderPlacedEventHandler.cs
+++ src/Order/DomainCore/SaleOrders.Applications/DomainEventHandlers/OrderPlacedEventHandler.cs
@@ -5,9 +5,9 @@ namespace SaleOrders.Applications.DomainEventHandlers;
 
 public class OrderPlacedEventHandler
 {
-    public async Task Handle(OrderPlacedDomainEvent domainDomainEvent, ILogger _logger, CancellationToken cancellationToken)
+    public static async Task HandleAsync(OrderPlacedDomainEvent domainDomainEvent, ILogger logger, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("收到訂單的領域事件 {OrderId}：{Product} x{Qty}",
+        logger.LogInformation("收到訂單的領域事件 {OrderId}：{Product} x{Qty}",
                                domainDomainEvent.OrderId, domainDomainEvent.ProductName, domainDomainEvent.Quantity);
     }
 }
